95fc4d9e40385c74ca1f84c186cbe5c5e0f6151a
Egor <egor@serverice.ru>
2020-10-20 17:09:23 +0700

    добавил модули разных экранов

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,5 +1,8 @@
 import React from "react";
 import PropTypes from "prop-types";
+import BrowserRouter from "react-router-dom";
+import Switch from "react-router-dom";
+import Route from "react-router-dom";
 import WelcomeScreen from "../welcome-screen/welcome-screen";
 
 const App = (props) => {
--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,7 @@ const path = require('path');
 
 module.exports = {
   entry: './src/index.js',
+  historyApiFallback: true,
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'public')
